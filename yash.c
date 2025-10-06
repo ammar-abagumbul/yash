3115d44839c7e70cbf7aa766538c66e3943413b5
ammar-abagumbul <ammar96@connect.hku.hk>
2025-10-06 13:13:42 +0800

    feat: propagate signals and report back result

--- yash.c
+++ yash.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/ioctl.h>
+#include <termios.h>
 
 #define PROMPT "## 3230yash >> "
 #define MAX_CMDS 30
@@ -11,7 +14,13 @@
 typedef struct {
   int pipe_count;
   char **pipes;
+  pid_t pids[MAX_CMDS];
 } Command;
+
+void handle_SIGINT(int sig) {
+  printf("\n");
+  signal(SIGINT, handle_SIGINT);
+}
 
 void free_tokens(char **tokens, int num_commands) {
   if (tokens == NULL || num_commands <= 0) {
@@ -228,8 +237,31 @@ int readline(char *buffer, size_t size) {
   }
 }
 
+void init_commands(Command *cmd, char* buffer) {
+  // Initialize commands here
+  memset(cmd->pids, 0, sizeof(cmd->pids));
+  tokenize(cmd, buffer);
+}
+
 int main(int argc, char *argv[]) {
   char buffer[1025];
+  pid_t master_pid = getpid();
+
+  // create a session
+  /*
+  if (setsid() == -1) {
+    perror("Fatal error creating session");
+    exit(EXIT_FAILURE);
+  }
+
+  if (ioctl(STDIN_FILENO, TIOCSCTTY, 0) == -1) {
+    perror("ioctl TIOCSCTTY failed");
+    exit(EXIT_FAILURE);
+  }
+  */
+
+  signal(SIGINT, handle_SIGINT);
+
   do {
     printf("%s", PROMPT);
 
@@ -239,7 +271,7 @@ int main(int argc, char *argv[]) {
         continue;
 
       Command cmd;
-      tokenize(&cmd, buffer);
+      init_commands(&cmd, buffer);
 
       // create pipes
       int pipes[cmd.pipe_count][2];
@@ -253,6 +285,7 @@ int main(int argc, char *argv[]) {
       for (int i = 0; i < cmd.pipe_count; i++) {
         pid_t pid = fork();
         if (pid == 0) {
+
           if (i == 0 && cmd.pipe_count > 1) {
             dup2(pipes[i][1], STDOUT_FILENO);
           } else if (i == cmd.pipe_count - 1) {
@@ -261,16 +294,29 @@ int main(int argc, char *argv[]) {
             dup2(pipes[i][1], STDOUT_FILENO);
             dup2(pipes[i - 1][0], STDIN_FILENO);
           }
+
+          if (i == 0){
+            setpgid(0, 0);
+          } else {
+            setpgid(0, cmd.pids[0]);
+          }
+
+          // restore SIGINT handler to default
+          signal(SIGINT, SIG_DFL);
+
           // close all fds in child
           for (int k = 0; k < cmd.pipe_count; k++) {
             close(pipes[k][0]);
             close(pipes[k][1]);
           }
+
           char **args = parse_args(cmd.pipes[i]);
           if (execvp(args[0], args) == -1) {
             perror("Fatal error executing command");
             exit(EXIT_FAILURE);
           }
+        } else if (pid > 0) {
+          cmd.pids[i] = pid;
         } else if (pid < 0) {
           perror("Fatal error creating child process");
           exit(EXIT_FAILURE);
@@ -285,15 +331,26 @@ int main(int argc, char *argv[]) {
 
       // wait for all children to finish
       for (int i = 0; i < cmd.pipe_count; i++) {
-        waitpid(-1, NULL, 0);
+        int status;
+        waitpid(cmd.pids[i], &status, 0);
+
+        if (WIFSIGNALED(status)) {
+          int sig = WTERMSIG(status);
+          printf("Child %d terminated by signal %d (%s)\n", i+1, sig, strsignal(sig));
+
+          if (WCOREDUMP(status))
+            printf(" (Core dumped)\n");
+        } else if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
+          printf("Child %d exited with status %d\n", i+1, WEXITSTATUS(status));
+        }
       }
 
       // free tokens
       free_tokens(cmd.pipes, cmd.pipe_count);
     }
-    // cleanup buffer
+
+    // cleanup buffer and take control of terminal
     memset(buffer, 0, sizeof(buffer));
-    putc('\n', stdout);
   } while (1);
 
   return 0;
