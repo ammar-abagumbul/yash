54a979e755044de46df090ab7f6d94d1096e4e77
ammar-abagumbul <ammar96@connect.hku.hk>
2025-10-08 20:29:48 +0800

    chore: refafactor code into reusable utility functions

--- utils.c
+++ utils.c
@@ -26,3 +26,34 @@ void print_tokens(char *tokens) {
   putc('0', stdout);
   putc('\n', stdout);
 }
+
+int count_args(const char* p) {
+  int count = 0;
+  while (*p) {
+    count++;
+    do {
+      p++;
+    } while (*p != '\0' && p[-1] != '\0');
+    p++;
+    if (*p == '\0')
+      break;
+  }
+  return count;
+}
+
+void register_for_report(const pid_t* pids, int count) {
+  for (int i = 0; i < count; i++) {
+    int status;
+    waitpid(pids[i], &status, 0);
+
+    if (WIFSIGNALED(status)) {
+      int sig = WTERMSIG(status);
+      printf("Child %d terminated by signal %d (%s)\n", i+1, sig, strsignal(sig));
+
+      if (WCOREDUMP(status))
+        printf(" (Core dumped)\n");
+    } else if (WIFEXITED(status) && WEXITSTATUS(status) != 0 && WEXITSTATUS(status) != 127) {
+      printf("Child %d exited with status %d\n", i+1, WEXITSTATUS(status));
+    }
+  }
+}
